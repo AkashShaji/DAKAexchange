e2f196960ed4a9963d779e125221a65b3efd9c73
Akash Shaji <akashshaji321@gmail.com>
2018-01-13 12:21:39 -0500

    added pythong side

---
+++ source/controllers/loginViewController.java
@@ -0,0 +1,10 @@
+package controllers;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+public class loginViewController   {
+    @FXML
+
+
+}
