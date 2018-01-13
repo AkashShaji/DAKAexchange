b2b735d50f96cbe8999bb6f157ada7ffde8e1528
James Taylor <james.taylor2@comcast.net>
2018-01-12 22:21:16 -0500

    Implemented FXML Loaders

--- source/controllers/MainWindowController.java
+++ source/controllers/MainWindowController.java
@@ -1,17 +1,34 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
-public class MainWindowController
-{
-    public MainWindowController(){
+public class MainWindowController {
+    @FXML
+    AnchorPane offers, account;
+    OfferTabController offerTabController;
+    AccountTabController accountTabController;
 
+    public MainWindowController(){
+        offerTabController = new OfferTabController();
+        accountTabController = new AccountTabController();
     }
 
     @FXML
     protected void initialize() throws IOException{
-
+        // Loads the offer menu
+        FXMLLoader offerLoader = new FXMLLoader(getClass().getResource("/resources/view/OfferTabView.fxml"));
+        offerLoader.setRoot(offers);
+        offerLoader.setController(offerTabController);
+        offerLoader.load();
+        // Loads the account menu
+        FXMLLoader accountLoader = new FXMLLoader(getClass().getResource("/resources/view/AccountTabView.fxml"));
+        accountLoader.setRoot(account);
+        accountLoader.setController(accountTabController);
+        accountLoader.load();
     }
 }
---
+++ source/controllers/OfferTabController.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class OfferTabController {
+    @FXML VBox Offers;
+
+}
